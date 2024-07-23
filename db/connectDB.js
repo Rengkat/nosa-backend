d016a22426dd00ecf80f18cc243287a73ddc9356
Rengkat <successinmotion11@gmail.com>
2024-07-23 08:57:12 +0100

    connect to db added

---
+++ db/connectDB.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+
+const connectDB = async (url) => {
+  return await mongoose.connect(url);
+};
+module.exports = connectDB;
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // imports
 const express = require("express");
+const connectDB = require("./db/connectDB");
 //loading .env
 require("dotenv").config();
 const app = express();
@@ -11,6 +12,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const start = async () => {
   try {
+    await connectDB(process.env.MONGODB_URL);
     app.listen(port, () => console.log(`Server running at port ${port}`));
   } catch (error) {}
 };
