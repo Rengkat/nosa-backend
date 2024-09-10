f686385096613a7b33d960adcbefd181b6db01a2
Rengkat <successinmotion11@gmail.com>
2024-09-10 17:45:40 +0100

    stats complete

--- controllers/statsController.js
+++ controllers/statsController.js
@@ -1,5 +1,20 @@
 const User = require("../model/userModel");
 const NosaSet = require("../model/setModel");
 const BlogsAndNews = require("../model/newsAndBlogModel");
-const getStats = async (req, res, next) => {};
+const { StatusCodes } = require("http-status-codes");
+
+const getStats = async (req, res, next) => {
+  try {
+    const totalMembers = await User.countDocuments({ isVerified: true });
+    const totalSets = await NosaSet.countDocuments();
+    const TotalBlogsAndNews = await BlogsAndNews.countDocuments({
+      category: { $in: ["news", "blog"] },
+    });
+    const events = await BlogsAndNews.countDocuments({ category: "event" });
+    res.status(StatusCodes.OK).json({ totalMembers, totalSets, TotalBlogsAndNews, events });
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = getStats;
