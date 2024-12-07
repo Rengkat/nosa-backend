6177b638cb01e7f45485c267229e94ba5628bc0f
Rengkat <successinmotion11@gmail.com>
2024-12-07 08:02:28 +0100

    set Media controllere added

---
+++ controllers/setDiscussionController.js
@@ -0,0 +1,13 @@
+const CustomError = require("../errors");
+const SetPost = require("../model/setPostModel");
+const { StatusCodes } = require("http-status-codes");
+const sendMessage = async (req, res, next) => {};
+const getAllSetMessage = async (req, res, next) => {};
+const updateMessage = async (req, res, next) => {};
+const deleteMessage = async (req, res, next) => {};
+module.exports = {
+  sendMessage,
+  getAllSetMessage,
+  updateMessage,
+  deleteMessage,
+};
---
+++ controllers/setMediaController.js
@@ -0,0 +1,10 @@
+const CustomError = require("../errors");
+const SetPost = require("../model/setPostModel");
+const { StatusCodes } = require("http-status-codes");
+
+const addImage = async (req, res, next) => {};
+const deleteImage = async (req, res, next) => {};
+module.exports = {
+  addImage,
+  deleteImage,
+};
