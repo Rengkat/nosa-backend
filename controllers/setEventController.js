6c68d6ea55fee539ed02e0c02df4af372d42d21d
Rengkat <successinmotion11@gmail.com>
2024-12-07 07:56:51 +0100

    set Event controllere added

---
+++ controllers/setEventController.js
@@ -0,0 +1,15 @@
+const CustomError = require("../errors");
+const SetPost = require("../model/setPostModel");
+const { StatusCodes } = require("http-status-codes");
+const createSetEvent = async (req, res, next) => {};
+const getAllSetEvent = async (req, res, next) => {};
+const getSingleSetEvent = async (req, res, next) => {};
+const updateSetEvent = async (req, res, next) => {};
+const deleteSetEvent = async (req, res, next) => {};
+module.exports = {
+  createSetEvent,
+  getAllSetEvent,
+  getSingleSetEvent,
+  updateSetEvent,
+  deleteSetEvent,
+};
--- controllers/setPostCommentController.js
+++ controllers/setPostCommentController.js
@@ -4,4 +4,4 @@ const { StatusCodes } = require("http-status-codes");
 const addPostComment = async (req, res, next) => {};
 const getAllPostComments = async (req, res, next) => {};
 const deletePostComment = async (req, res, next) => {};
-module.export = { addPostComment, getAllPostComments, deletePostComment };
+module.exports = { addPostComment, getAllPostComments, deletePostComment };
