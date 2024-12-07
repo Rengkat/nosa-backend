288d8f27dd1c23cda70f55c6c33837a8a53632d9
Rengkat <successinmotion11@gmail.com>
2024-12-07 05:53:52 +0100

    post comments model added

---
+++ model/postCommentModel.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const CommentSchema = new mongoose.Schema(
+  {
+    post: { type: mongoose.Schema.Types.ObjectId, ref: "Post", required: true },
+    author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    content: { type: String, required: [true, "Comment content is required"] },
+    replies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Comment = mongoose.model("Comment", CommentSchema);
+
+module.exports = Comment;
