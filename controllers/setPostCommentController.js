6e82207cc1e5091caab47749c18c3072f5b59879
Rengkat <successinmotion11@gmail.com>
2024-12-07 07:46:00 +0100

    set Group Post controller get all post

---
+++ controllers/setPostCommentController.js
@@ -0,0 +1,7 @@
+const CustomError = require("../errors");
+const SetPost = require("../model/setPostModel");
+const { StatusCodes } = require("http-status-codes");
+const addPostComment = async (req, res, next) => {};
+const getAllPostComments = async (req, res, next) => {};
+const deletePostComment = async (req, res, next) => {};
+module.export = { addPostComment, getAllPostComments, deletePostComment };
