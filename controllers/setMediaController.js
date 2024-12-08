015f54e5cd03c606c69b0fb0c607ffbd721cb05f
Rengkat <successinmotion11@gmail.com>
2024-12-08 20:05:17 +0100

    upload media image controller added

--- controllers/setMediaController.js
+++ controllers/setMediaController.js
@@ -4,7 +4,35 @@ const { StatusCodes } = require("http-status-codes");
 
 const addImage = async (req, res, next) => {};
 const deleteImage = async (req, res, next) => {};
+const uploadSetMediaImage = async (req, res, next) => {
+  try {
+    if (!req.files || !req.files.image) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: "No image file uploaded", success: false });
+    }
+
+    if (!fs.existsSync(req.files.image.tempFilePath)) {
+      // the file path
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: "File not found", success: false });
+    }
+
+    const result = await cloudinary.uploader.upload(req.files.image.tempFilePath, {
+      use_filename: true,
+      folder: process.env.CLOUDINARY_SET_MEDIA_FOLDER_NAME,
+    });
+
+    fs.unlinkSync(req.files.image.tempFilePath);
+
+    return res.status(StatusCodes.CREATED).json({ mediaImgUrl: result.secure_url, success: true });
+  } catch (error) {
+    next(error);
+  }
+};
 module.exports = {
   addImage,
   deleteImage,
+  uploadSetMediaImage,
 };
