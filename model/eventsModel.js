96f7aa00aa68e40c6e6cfcf181d4d40c234c4c0c
Rengkat <successinmotion11@gmail.com>
2025-02-17 05:40:45 +0100

    adjustevent model

--- model/eventsModel.js
+++ model/eventsModel.js
@@ -5,8 +5,21 @@ const EventSchema = new mongoose.Schema(
     image: { type: String, required: [true, "Please provide image link"] },
     title: { type: String, required: [true, "Please provide the title"], maxLength: 100 },
     description: { type: String },
+    dateOfEvent: {
+      type: Date,
+      required: [true, "Please provide the date of the event"],
+    },
+    venue: {
+      name: { type: String, required: [true, "Please provide the venue name"] },
+      address: { type: String, required: [true, "Please provide the venue address"] },
+      city: { type: String, required: [true, "Please provide the city"] },
+      state: { type: String, required: [true, "Please provide the state"] },
+      country: { type: String, required: [true, "Please provide the country"] },
+      zipCode: { type: String, required: [true, "Please provide the zip code"] },
+    },
     content: { type: String, required: [true, "Please provide the content"] },
   },
   { timestamps: true }
 );
+
 module.exports = mongoose.model("Event", EventSchema);
